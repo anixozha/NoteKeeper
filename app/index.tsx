4c2ef84cd92f5bb8508340c49e60cd4d7adfa79c
anixozha <anishbabuojha@gmail.com>
2024-06-25 18:11:17 +0545

    update

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,43 @@
-import { View, Text } from 'react-native'
+import { View, Text ,StyleSheet, TextInput, Button } from 'react-native'
 import React from 'react'
-import Home from './home'
+import { useState } from 'react'
+
 import { Link } from 'expo-router'
 
 const Main = () => {
+  const[text, setText]= useState<String>("")
   return (
-<View>
-    <Link href={{pathname:'/home',params:{Name:'Anish Babu Ojha'}}}>Home</Link>
-    <Link href={'/about'}>About</Link>
+<View style={styles.container}>
+
+<Text style={styles.text} > Type anything!</Text>
+<TextInput multiline={true} style={styles.textInput} value={text} onChangeText={setText}/> 
+<Button title='Save Note' onPress={()=>console.log('save the note', text)}/>
 </View>
 
-  )
+  );
 
 }
 
 export default Main
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+    backgroundColor: '#fff',
+    alignItems :'center',
+    justifyContent:'center',
+  },
+  textInput:{
+    backgroundColor:"#ffb70342",
+    width:"100%",
+    height:200,
+    fontSize: 26,
+    paddingHorizontal:20,
+    paddingTop:20,
+    paddingBottom:20,
+    textAlignVertical:'top'
+  
+  },
+  text:{
+    fontSize:18
+  }
+})
